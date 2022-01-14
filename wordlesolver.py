50537e686b8b2ca5ca91732b84bb535750d3afb5
jackyrobot <jacky.robot321@gmail.com>
2022-01-14 00:35:28 -0800

    Fixed bugs and rejects invalid inputs

--- wordlesolver.py
+++ wordlesolver.py
@@ -1,41 +1,58 @@
 
-with open("words.txt", 'r') as f:
+with open("newwords_dupesremoved.txt", 'r') as f:
 	words = [line.rstrip() for line in f]
 
 firstguess = "tares"
 whitelist = set()
 blacklist = set()
-posblacklist = {0: [], 1: [], 2: [], 3: [], 4: []}
+posblacklist = {0: [], 1: [], 2: [], 3: [], 4: []} # positional blacklist
 res = ["", "", "", "", ""]
 c = 0
-# for word in words:
-# 	print(word, c)
-# 	c += 1
 
 while len(words) > 1:
+
+	# print out words if less than 7 possible words left
+	if len(words) < 7:
+		r = ""
+		for word in words:
+			r += word + " "
+		print("Possible words remaining: " + r)
 	curr = words[0]
 	inp = input("Please input " + curr + " and enter result: ")
+
+	# check valid input
+	valid = True
+	if len(inp) != 5:
+		valid = False
+	for letter in inp:
+		if letter not in {'0', '1', '2'}:
+			valid = False
+	if valid == False:
+		print("Not a valid input. Please try again.")
+		continue
+
+	# go through input and add to whitelist/blacklist
 	for i in range(len(inp)):
 		if inp[i] == "0":
 			blacklist.add(curr[i])
 		elif inp[i] == "1":
 			whitelist.add(curr[i])
-			print(i, curr[i])
-			posblacklist[i].append(curr[i])
+			posblacklist[i].append(curr[i]) # if yellow letter, must add letter to blacklist for this position
 		elif inp[i] == "2":
 			res[i] = curr[i]
 			whitelist.add(curr[i])
 		else:
 			print("Not a valid input. Please try again.")
 			continue
+
+	# filter words from dictionary
 	i = 0
 	while i < len(words):
+
 		breaked = False
+
 		# check if word has a blacklisted letter
-		if len(words) < 20:
-			print(words, i)
 		for j in range(len(words[i])):
-			print(j)
 			if words[i][j] in blacklist:
 				words.pop(i)
 				# i -= 1
@@ -43,6 +60,7 @@ while len(words) > 1:
 				break
 		if breaked: 
 			continue
+
 		# check to make sure word contains all of whitelisted letters
 		for let in whitelist:
 			if let not in words[i]:
@@ -51,6 +69,7 @@ while len(words) > 1:
 				break
 		if breaked: 
 			continue
+
 		# check to make sure word has letters in established positions
 		for k in range(len(res)):
 			if res[k] != "":
@@ -60,6 +79,7 @@ while len(words) > 1:
 					break
 		if breaked:
 			continue
+
 		# check to make sure yellow letters are not repeated
 		for l in range(len(words[i])):
 			if words[i][l] in posblacklist[l]:
@@ -69,7 +89,11 @@ while len(words) > 1:
 		if breaked:
 			continue
 		i += 1
-print("The result is: " + words[0])
+
+if len(words) < 1:
+	print("No solution found.")
+else:
+	print("The result is: " + words[0])
 
 
 
