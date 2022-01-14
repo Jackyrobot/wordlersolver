d8a87c02319453ec63b850b391e7760ed2bd4f2b
jackyrobot <jacky.robot321@gmail.com>
2022-01-14 00:44:06 -0800

    fixed filenames

--- removedupe.py
+++ removedupe.py
@@ -1,8 +1,15 @@
-with open("newwords.txt", 'r') as f:
-	words = [line.rstrip() for line in f]
-	res = []
-	[res.append(x) for x in words if x not in res]
-	
-with open("newwords_dupesremoved.txt", 'w') as g:
-	for word in res:
-		g.write(word + "\n")
+import os
+import sys
+
+if __name__ == '__main__':
+    fromfile = sys.argv[1]
+    tofile = sys.argv[2]
+
+    with open(fromfile, 'r') as f:
+        words = [line.rstrip() for line in f]
+        res = []
+        [res.append(x) for x in words if x not in res]
+        
+    with open(tofile, 'w') as g:
+        for word in res:
+            g.write(word + "\n")
